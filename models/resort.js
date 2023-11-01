c6bc9a495071fd06d9f74ea3185e54a8b65bb3bb
Patrick_Andrada <patrickanthonio993@gmail.com>
2023-11-01 22:18:53 +0800

    initial commit

---
+++ models/resort.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+const resortSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Resort = mongoose.model("Resort", resortSchema);
